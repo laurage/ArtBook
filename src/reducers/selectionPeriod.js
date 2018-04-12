3544a50c2661842075ab776e249c8545f54c814d
laurage <laurage_@hotmail.com>
2018-04-12 21:33:39 +0100

    added comments to the selection file

--- src/reducers/selectionPeriod.js
+++ src/reducers/selectionPeriod.js
@@ -15,12 +15,12 @@ export const selectionPeriod = (state = initialState, { type, selectedDay } = {}
         return Object.assign({}, state, {
           selectedDaySecond: null,
         })
-      } else if (state.selectedDayFirst === null && (state.selectedDaySecond && selectedDay.isAfter(state.selectedDaySecond))) {
+      } else if (state.selectedDayFirst === null && (state.selectedDaySecond && selectedDay.isAfter(state.selectedDaySecond))) { // if user clicked on 1st date and then selects a day after the last date selected
         return Object.assign({}, state, {
           selectedDayFirst: state.selectedDaySecond,
           selectedDaySecond: selectedDay,
         })
-      } else if (state.selectedDayFirst === null || (state.selectedDayFirst && selectedDay.isBefore(state.selectedDayFirst))) {
+      } else if (state.selectedDayFirst === null || (state.selectedDayFirst && selectedDay.isBefore(state.selectedDayFirst))) { // if no date has been selected yet, or first date has been selected and user then selects a date before that date
         return Object.assign({}, state, {
           selectedDayFirst: selectedDay,
         })
